77142a2c07d8bb4ce6fff7cb44aaf1def78ec6ed
Edgar Chavez <edgarchavez@Edgars-MacBook-Air.local>
2023-10-14 12:59:04 -0600

    add maincontainer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import MainContainer from "./components/MainContainer";
 
 const App = () => {
   return (
     <>
       <Header />
+      <MainContainer />
     </>
   );
 };
---
+++ frontend/src/components/MainContainer/MainContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./MainContainer.module.scss";
+
+const MainContainer = () => {
+  return (
+    <div className={styles.mainContainer}>
+      <p>Welcome to Avini</p>
+    </div>
+  );
+};
+
+export default MainContainer;
