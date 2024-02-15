119b370dd2da76bf1848d2c109a80a2123a3886a
Edgar Chavez <edgarchavez@Edgars-MacBook-Air.local>
2024-02-15 17:05:24 -0600

    import  createBrowserRouter,   createRoutesFromElements   Route   RouteProvider   RouterProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouteProvider,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App />}>
+      {/**Index attribute to avoid rendering multiple screens */}
+      <Route index={true} path='/' element={<HomeScreen />} />
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
