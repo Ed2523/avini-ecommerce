3fea36334e92e35515fbfda2ab82d0c255ea4bcf
Edgar Chavez <edgarchavez@Edgars-MacBook-Air.local>
2024-02-19 12:11:10 -0600

    add rating component

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -1,15 +1,19 @@
 import React from "react";
 import styles from "./Product.module.scss";
 import { Link } from "react-router-dom";
+import Rating from "../Rating";
 
 const Product = ({ product }) => {
   return (
     <div className={styles.card}>
       <Link to={`/product/${product._id}`}>
         <img src={product.image}></img>
-        <a>{product.name}</a>
-        <p>${product.price}</p>
       </Link>
+      <Link>
+        <p>{product.name}</p>
+      </Link>
+      <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+      <p>${product.price}</p>
     </div>
   );
 };
---
+++ frontend/src/components/Rating/Rating.js
@@ -0,0 +1,63 @@
+import React from "react";
+import styles from "./Rating.module.scss";
+import { FaStar, FaStarHalfAlt, FaRegStar } from "react-icons/fa";
+
+const Rating = ({ value, text }) => {
+  return (
+    <div className={styles.rating}>
+      <span>
+        {/*refactor this into a more simple expression*/}
+        {value >= 1 ? (
+          <FaStar />
+        ) : value >= 0.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {/*refactor this into a more simple expression*/}
+        {value >= 2 ? (
+          <FaStar />
+        ) : value >= 1.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {/*refactor this into a more simple expression*/}
+        {value >= 3 ? (
+          <FaStar />
+        ) : value >= 2.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {/*refactor this into a more simple expression*/}
+        {value >= 4 ? (
+          <FaStar />
+        ) : value >= 3.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span>
+        {/*refactor this into a more simple expression*/}
+        {value >= 5 ? (
+          <FaStar />
+        ) : value >= 4.5 ? (
+          <FaStarHalfAlt />
+        ) : (
+          <FaRegStar />
+        )}
+      </span>
+      <span className={styles.ratingText}>{text && text}</span>
+    </div>
+  );
+};
+
+export default Rating;
