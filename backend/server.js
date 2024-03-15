cc3f1f0c80fa28329751cf12416f71f2a749ee6b
Edgar Chavez <citylight2392@gmail.com>
2024-03-15 06:29:08 -0600

    add api route to get product by id

--- backend/server.js
+++ backend/server.js
@@ -15,4 +15,8 @@ app.get("/api/products", (req, res) => {
   res.json(products);
 });
 
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((product) => product._id === req.params.id);
+  res.json(product);
+});
 app.listen(port, () => console.log(`Server running on port ${port}`));
