8812357405ee9d1c4b2c8aec9f7e7eee29310886
“Ed2523” <citylight2392@gmail.com>
2024-07-28 17:00:44 -0600

    add constans

---
+++ frontend/src/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL =
+  process.send.NODE_ENV === "development" ? "http://localhost:8000" : "";
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const PAYPAL_URL = "/api/users";
